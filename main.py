23840721e974ba37c7c148155ce67f7e3a07d54c
Jonathan Alvaro <jonalvarot@gmail.com>
2019-05-04 10:28:09 +0700

    Added function to automatically run a hash algorithm iteratively

---
+++ main.py
@@ -0,0 +1,20 @@
+import hashlib
+import itertools
+
+hash_algorithms = hashlib.algorithms_available
+
+def run_hash_algorithm(hash_func, seed=b"Default seed", num_iters=100):
+    """
+    Generates random numbers using 100 instances of the algorithm
+    hash_func: One of the hash functions in the hashlib module
+    """
+
+    hasher = hash_func(seed)
+    current_string = hasher.hexdigest()
+    numbers = []
+    for _ in range(num_iters):
+        for j, character in enumerate(current_string):
+            if int(character,base=16) >= 10:
+                continue
+            numbers.append(int(character, base=16))
+    return numbers
